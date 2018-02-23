29a1d26300c54637ba4edf7a6b24294bfc269c7f
zihe <hezihao@linghit.com>
2018-02-23 17:06:01 +0800

    添加内存泄露监控。

---
+++ sample/src/main/java/com/wally/android/wait/sample/AppContext.java
@@ -0,0 +1,25 @@
+package com.wally.android.wait.sample;
+
+import android.app.Application;
+
+import com.squareup.leakcanary.LeakCanary;
+
+/**
+ * Package: com.wally.android.wait.sample
+ * FileName: AppContext
+ * Date: on 2018/2/23  下午5:00
+ * Auther: zihe
+ * Descirbe:
+ * Email: hezihao@linghit.com
+ */
+
+public class AppContext extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            return;
+        }
+        LeakCanary.install(this);
+    }
+}
